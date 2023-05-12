917d4804d94bb2880fc8f58a35a8a10758764a05
Schalk van Dyk <schalkvandyk1979@gmail.com>
2023-05-12 15:52:24 +0200

    add styling, hover effects and animation to navlist and navitems

--- app.js
+++ app.js
@@ -16,45 +16,56 @@ const navItem5 = document.querySelector('.navItem5');
 
 hamburgerBtn.addEventListener('click', () => {
   
-  hamburgerTopBar.classList.toggle('hamburgerBtn__hamburgerBar--right');
-  hamburgerMiddleBar.classList.toggle('hamburgerBtn__hamburgerBar--transparent');
-  hamburgerBottomBar.classList.toggle('hamburgerBtn__hamburgerBar--left');
+  if (!hamburgerTopBar.classList.contains('hamburgerBtn__hamburgerBar--right')) {
+    setTimeout(() => {
+      hamburgerTopBar.classList.toggle('hamburgerBtn__hamburgerBar--right');
+      hamburgerMiddleBar.classList.toggle('hamburgerBtn__hamburgerBar--transparent');
+      hamburgerBottomBar.classList.toggle('hamburgerBtn__hamburgerBar--left');
+    }, 200);
+  } else {
+    setTimeout(() => {
+      hamburgerTopBar.classList.toggle('hamburgerBtn__hamburgerBar--right');
+      hamburgerMiddleBar.classList.toggle('hamburgerBtn__hamburgerBar--transparent');
+      hamburgerBottomBar.classList.toggle('hamburgerBtn__hamburgerBar--left');
+    }, 500);
+  }
   
-//   if(hamburgerMenu.classList.contains('navList--displayNone')) {
-//     hamburgerMenu.classList.remove('navList--displayNone');
-//     hamburgerMenu.classList.add('navList--display');
-//     for(let item of navItems) {
-//       item.style.opacity = "0";
-//     }
-//     setTimeout(() => {
-//       hamburgerMenu.classList.remove('navList--navListMoveOut');
-//       hamburgerMenu.classList.add('navList--navListMoveIn');
-//       for(let item of navItems) {
-//         item.classList.remove('navItem--moveOut');
-//         item.classList.add('navItem--moveIn');
-//       }
-//       navItem1.style.animationDelay = '.5s';
-//       navItem2.style.animationDelay = '.75s';
-//       navItem3.style.animationDelay = '1s';
-//       navItem4.style.animationDelay = '1.25s';
-//       navItem5.style.animationDelay = '1.5s';
-//     }, 20);
-//   } else {
-//     hamburgerMenu.classList.remove('navList--navListMoveIn')
-//     hamburgerMenu.classList.add('navList--navListMoveOut')
-//     for(let item of navItems) {
-//       item.style.opacity = "1";
-//       item.classList.remove('navItem--moveIn');
-//       item.classList.add('navItem--moveOut');
-//     }
-//     navItem1.style.animationDelay = '.25s';
-//     navItem2.style.animationDelay = '.5s';
-//     navItem3.style.animationDelay = '.75s';
-//     navItem4.style.animationDelay = '1s';
-//     navItem5.style.animationDelay = '1.25s';
-//     setTimeout(() => {
-//       hamburgerMenu.classList.remove('navList--display');
-//       hamburgerMenu.classList.add('navList--displayNone');
-//     }, 3000);
-//   }  
+  
+  if(hamburgerMenu.classList.contains('navList--displayNone')) {
+    hamburgerMenu.classList.remove('navList--displayNone');
+    hamburgerMenu.classList.add('navList--display');
+    for(let item of navItems) {
+      item.style.opacity = "0";
+    }
+    setTimeout(() => {
+      hamburgerMenu.classList.remove('navList--navListMoveOut');
+      hamburgerMenu.classList.add('navList--navListMoveIn');
+      for(let item of navItems) {
+        item.classList.remove('navItem--moveOut');
+        item.classList.add('navItem--moveIn');
+      }
+      navItem1.style.animationDelay = '.50s';
+      navItem2.style.animationDelay = '.70s';
+      navItem3.style.animationDelay = '.90s';
+      navItem4.style.animationDelay = '1.20s';
+      navItem5.style.animationDelay = '1.40s';
+    }, 20);
+  } else {
+    hamburgerMenu.classList.remove('navList--navListMoveIn')
+    hamburgerMenu.classList.add('navList--navListMoveOut')
+    for(let item of navItems) {
+      item.style.opacity = "1";
+      item.classList.remove('navItem--moveIn');
+      item.classList.add('navItem--moveOut');
+    }
+    navItem1.style.animationDelay = '.20s';
+    navItem2.style.animationDelay = '.30s';
+    navItem3.style.animationDelay = '.40s';
+    navItem4.style.animationDelay = '.50s';
+    navItem5.style.animationDelay = '.60s';
+    setTimeout(() => {
+      hamburgerMenu.classList.remove('navList--display');
+      hamburgerMenu.classList.add('navList--displayNone');
+    }, 2000);
+  }  
 });
